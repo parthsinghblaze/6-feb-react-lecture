a6adba87796d5c0dd51752eaf3a78a1e1722ad7a
parthsingh <parth@gmail.com>
2023-03-03 08:26:35 +0530

    initial commit

---
+++ src/Page.js
@@ -0,0 +1,14 @@
+// Component is basically a function which return html/ JSX 
+
+// to create a component, you can use any  function syntax, but function name should in Capitalize 
+
+
+function Page() {
+    return (
+        <div>
+            <h1>Page Component</h1>
+        </div>
+    )
+}
+
+export default Page
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from 'react-dom'
+import Page from './Page'
+import Header from './Header'
+
+// 1-> node js install
+// 2-> To check the version of node user [node -v]
+// 3-> to create react app use [npx create-react-app]
+// 4-> to install all the dependance use [npm install]
+// 5-> to start the react use [npm run start/ npm start]
 
 // 1 
 let htmlElement = document.getElementById('root')
@@ -8,4 +16,4 @@ let htmlElement = document.getElementById('root')
 let root = ReactDOM.createRoot(htmlElement)
 
 // 3
-root.render(<h1>Hello World</h1>)
+root.render(<section><Page /> <Header /></section>)
